2b6ffcca9741544bae7c7c823790778a62083c34
Anton Lykov <antony.lykov@gmail.com>
2019-10-25 19:30:42 -0700

    Implement basic sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  get '/show', to: 'teams#show'
   root 'splash_screen#join'
+
+  get '/show', to: 'teams#show'
   get '/join', to: 'splash_screen#join'
   get '/create', to: 'splash_screen#create'
+  get '/signup', to: 'users#new'
+
+  # FIXME: Need to change path later.
+  get 'teams/new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :teams
+  resources :users
 end
