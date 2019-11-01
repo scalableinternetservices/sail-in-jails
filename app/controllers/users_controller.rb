a33005cb6ab39dda6bba484fb53a1a40984add79
Anton Lykov <antony.lykov@gmail.com>
2019-11-01 15:50:20 -0700

    Add user password in mvc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,8 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 
 end
