a25b59b0fa51767737399805ada9130ad6365736
Anton Lykov <antony.lykov@gmail.com>
2019-11-01 15:51:34 -0700

    Implement sessions

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get 'sessions/new'
+    assert_response :success
+  end
+
+end
