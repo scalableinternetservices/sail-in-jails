4c5169c9f13284d46c2f942eb03830d9a6537128
Anton Lykov <antony.lykov@gmail.com>
2019-11-08 12:26:26 -0800

    Add basic create team form

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,25 +1,26 @@
 class TeamsController < ApplicationController
-  
+
   def new
-    @team = Team.new 
+    @team = Team.new
   end
-  
+
   def show
-    #@team = Team.find(params[:id])
-  end 
-  
-  def create 
+    @team = Team.find(params[:id])
+  end
+
+  def create
     @team = Team.new(team_params)
     if @team.save
       render 'show'
     else
+      flash.now[:danger] = 'Invalid info!'
       render 'new'
-    end 
-  end 
-  
+    end
+  end
+
   private
   def team_params
     params.require(:team).permit(:name, :desc, :course)
-  end 
-  
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,17 @@ Rails.application.routes.draw do
   get '/courses', to: 'courses#new'
   get '/join', to: 'splash_screen#join'
   get '/join/:id', to: 'splash_screen#join'
-  get '/create', to: 'splash_screen#create'
+
   get '/signup', to: 'users#new'
   get '/profile', to: 'splash_screen#profile'
+
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  # FIXME: Need to change path later.
-  get 'teams/new'
+
+
+  get '/create', to: 'teams#new'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :teams
