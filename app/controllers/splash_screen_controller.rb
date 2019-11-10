cc74d59f1959dc770462bce92b0c0131d3a567f5
joelwdav <joelwdavidson@gmail.com>
2019-11-09 21:00:49 -0800

    added so the join screen only shows the user you're logged in as

--- app/controllers/splash_screen_controller.rb
+++ app/controllers/splash_screen_controller.rb
@@ -2,6 +2,7 @@ class SplashScreenController < ApplicationController
   def join
     @id = params[:id]
     @course = Course.where(id: @id)[0]
+    @user = User.where(id: current_user.id)[0]
   end
 
   def create
