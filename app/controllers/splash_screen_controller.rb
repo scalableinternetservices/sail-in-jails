03f41bf85ac909aba8743485c7513968898f060f
joelwdav <joelwdavidson@gmail.com>
2019-11-14 16:45:38 -0800

    fixed join team page not working for logged out user

--- app/controllers/splash_screen_controller.rb
+++ app/controllers/splash_screen_controller.rb
@@ -2,7 +2,12 @@ class SplashScreenController < ApplicationController
   def join
     @id = params[:id]
     @course = Course.where(id: @id)[0]
-    @user = User.where(id: current_user.id)[0]
+
+    if logged_in?
+      @user = User.where(id: current_user.id)[0]
+    else
+      @user = User.new
+    end
   end
 
   def create
