5333512d2f14e3491c59c4194cb936de9e12b299
joelwdav <joelwdavidson@gmail.com>
2019-11-15 12:46:01 -0800

    fixed the course creation endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   get '/create', to: 'teams#new'
 
   # COURSE
-  get '/courses/create', to: 'courses#new'
+  get '/courses', to: 'courses#new'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :teams
