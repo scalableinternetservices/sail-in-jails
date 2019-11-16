fcdf3ebe05c367c51b12e32e69eba9ed2fb5a887
Anton Lykov <antony.lykov@gmail.com>
2019-11-15 16:48:30 -0800

    Cleanup tests and remove legacy files

--- test/controllers/splash_screen_controller_test.rb
+++ test/controllers/splash_screen_controller_test.rb
@@ -2,15 +2,9 @@ require 'test_helper'
 
 class SplashScreenControllerTest < ActionDispatch::IntegrationTest
   test "should get join" do
-    get join_path
+    get '/join'
     assert_response :success
     assert_select "title", "Join | Group App"
-  end
-
-  test "should get create" do
-    get '/create'
-    assert_response :success
-    assert_select "title", "New Team | Group App"
   end
 
 
