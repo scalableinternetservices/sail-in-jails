8ab098150ac1ad25e7bbea480c0bd8664416ca1d
joelwdav <joelwdavidson@gmail.com>
2019-11-15 17:08:34 -0800

    made join screen safer

--- app/helpers/splash_screen_helper.rb
+++ app/helpers/splash_screen_helper.rb
@@ -1,9 +1,9 @@
 module SplashScreenHelper
 
     def get_team_course_name(team)
-        course_name = Course.find(team.course).name
+        course_name = Course.find(team.course)
         if course_name
-          return course_name
+          return course_name.name
         else
           return "This team does not have a course!"
         end 
