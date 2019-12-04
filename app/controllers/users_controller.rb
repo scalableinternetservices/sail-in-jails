53eacc7b5207fa51e564e77833211bf54d0f9c1d
Anton Lykov <antony.lykov@gmail.com>
2019-12-03 19:45:27 -0800

    Fix user profile overheads

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,16 +10,16 @@ class UsersController < ApplicationController
   def index
     @user = User.find(params[:id])
   end
-  
+
   def edit_profile
     @user = User.find(params[:id])
   end
-  
+
   def update
     if logged_in? && current_user.id.to_s == params[:id]
       @user = User.find(params[:id])
       if @user.update_attributes!(user_update_params)
-        render 'profile'
+        redirect_to '/profile'
       else
         Rails.logger.info(@user.errors.messages.inspect)
         render 'edit_profile'
@@ -27,17 +27,22 @@ class UsersController < ApplicationController
     else
       redirect_to join_path
     end
-  end 
-  
-  
-  def profile 
+  end
+
+  def profile
     if logged_in?
-      @user = User.where(id: current_user.id)[0]
-      @team = Team.find_by(id: @user.team_id)
-    else 
+      @user = User.find_by_sql(["
+        SELECT u.name, u.email, u.bio, u.id, courses.name AS course_name, COALESCE(teams.name, 'N/A') AS team_name
+        FROM users as u
+        LEFT JOIN courses ON u.course = courses.id
+        LEFT JOIN teams ON u.team_id = teams.id
+        WHERE u.id = ?
+        LIMIT 1", current_user.id
+      ]).first
+    else
       redirect_to join_path
     end
-  end 
+  end
 
   def leave_team
     @user = User.where(id: current_user.id)[0]
@@ -63,7 +68,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation, :course)
     end
-    
+
     def user_update_params
       user_params = params.require(:user).permit(:name, :email, :bio, :password, :password_confirmation)
       user_params[:course] = @user.course
