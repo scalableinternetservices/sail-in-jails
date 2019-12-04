4302054dfa4ec959ccc5c1f58bfeb7a4150369d5
Siddharth Pandiya <siddharthpandiya95@gmail.com>
2019-12-03 16:06:02 -0800

    Skipping verify authenticity token

--- app/controllers/splash_screen_controller.rb
+++ app/controllers/splash_screen_controller.rb
@@ -1,4 +1,5 @@
 class SplashScreenController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def join
     @id = params[:id]
     @course = Course.where(id: @id)[0]
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,5 @@
 class TeamsController < ApplicationController
-
+  skip_before_action :verify_authenticity_token
   def new
     if logged_in?
       @team = Team.new
