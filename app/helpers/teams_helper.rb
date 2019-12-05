eb7c831b9cfb658d4d8a1564cb8c63e1ce1e280c
Anton Lykov <antony.lykov@gmail.com>
2019-12-04 17:33:16 -0800

    Minor modification with team members

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,10 +1,3 @@
 module TeamsHelper
-    def get_team_members
-        members = User.where(team_id: Team.find(params[:id]).id)
-        if members == nil 
-            return []
-        else
-            return members
-        end
-    end
+
 end
